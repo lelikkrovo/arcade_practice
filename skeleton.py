96978375d49f394c1b15d4a93a91cde15608b27a
vladimir <lelik.krovo@gmail.com>
2021-05-18 20:45:28 +0259

    add

---
+++ skeleton.py
@@ -0,0 +1,24 @@
+import arcade
+
+class Platformer(arcade.Window):
+    def __init__(self):
+        pass
+
+    def setup(self):
+        
+        pass
+
+    def on_key_press(self, key: int, modifiers: int):
+
+
+    def on_update(self, delta_time: float):
+
+        pass
+
+    def on_draw(self):
+        pass
+
+if __name__ == "__main__":
+    window = Plarformer()
+    window.setup()
+    arcade.run()
